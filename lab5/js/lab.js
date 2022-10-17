79f6518297820ab112d47eff5627c4a408e67744
museofmoods <jaq.moore13@gmail.com>
2022-10-17 10:46:54 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+*Author: Jaq and Mikayla
+*Created: 17 October
+*License: Public Domain
+/
+
+// Define Variables
+make = Volkswagen
+model = Golf
+color = Cherry Red
+year = 1995
+
+// Calculate
+age = 2022 - year
+
+// output
+document.writeln("Make: " + Volkswagen + "<br>");
+document.writeln("Model: " + Golf + "<br>");
+document.writeln("Color: " + Cherry Red + "<br>");
+document.writeln("Year: " + 1995 + "<br>");
+document.writeln("Age: " + 27 + "<br>");
