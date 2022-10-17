72926ede54a0a22a37e9f42a0c5b51c8ce5d7f72
museofmoods <jaq.moore13@gmail.com>
2022-10-17 10:28:49 -0700

    new

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+// A website that allows users to select a playlist that corresponds to a particular mood.
+// There are pictures that are linked and represent various vibes (chill, positive, angry, romantic).
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// How to make a playlist based off of specific moods
+    // First, make the playlists via spotify, apple music, or soundcloud/selected music streaming service
+        // Choose the mood that each playlist relates to
+            // Allow users to select whichever playlist corresponds to their mood
