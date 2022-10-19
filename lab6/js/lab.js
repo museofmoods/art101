b9c96df80ecbe301b1b1b9a2c5ee90d5b614c35e
museofmoods <jaq.moore13@gmail.com>
2022-10-19 14:21:24 -0700

    11

---
+++ lab6/js/lab.js
@@ -0,0 +1,24 @@
+/*
+*Author: Jaq and Mikayla
+*License: Public Domanin
+*/
+
+
+//Define Variables
+myTransport = ["Volkswagen Golf", "bus", "legs", "scooter"];
+
+myMainRide = {
+  make: "Volkswagen"
+  model: "Golf"
+  color: "Cherry Red"
+  year: 1995
+  age: function () {
+      return 2022 - age;
+
+  }
+}
+
+// Output
+document.writeln("KindsofTransportation I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>");
+    JSON.stringify("myMainRide, null,  </pre>");
